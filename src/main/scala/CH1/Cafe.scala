c1e22dcfdfd66bed215dad327e9822fe870cf26a
Ryan Murphy <rymurphy12@gmail.com>
2023-06-12 20:13:32 -0500

    made the code more functional

--- src/main/scala/CH1/Cafe.scala
+++ src/main/scala/CH1/Cafe.scala
@@ -2,24 +2,29 @@ package CH1
 
 
 class Cafe{
-    def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
+    def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
         val cup = Coffee()
-        p.charge(cc, cup.price)
-        cup
+        (cup, Charge(cc, cup.price))
+    }
+
+    def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+        val purchases = List.fill(n)(buyCoffee(cc))
+        val (coffees, charges) = purchases.unzip
+        val reduced = charges.reduce((c1, c2) => c1.combine(c2))
+        (coffees, reduced)
     }
         
 }
 
-trait Payments{
-    def charge(cc: CreditCard, price: Double): Unit
-}
-
-class SimulatedPayments extends Payments{
-    def charge(cc: CreditCard, price: Double): Unit = {
-        println("charging " + price + " to " + cc)
+case class Charge(cc: CreditCard, amount: Double){
+    def combine(other: Charge): Charge = {
+        if cc == other.cc then
+            Charge(cc, amount + other.amount)
+        else
+            throw Exception("Can't combine charges with different cards")
     }
-}        
-
+}
+   
 class Coffee {
     val price = 2.0
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,7 @@
 import CH1._
 @main def hello(): Unit = {
   val cc = CreditCard()
-  val p = SimulatedPayments()
   val cafe = Cafe()
-  val cup = cafe.buyCoffee(cc, p)
+  val cup = cafe.buyCoffee(cc)
 
 }
