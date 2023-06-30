ff96f1827fc4cc121d2bb94910fe97970abdb737
Ryan Murphy <rymurphy12@gmail.com>
2023-06-29 21:20:39 -0500

    Added more code and exercise 2.1 complete

--- src/main/scala/CH2/Intro.scala
+++ src/main/scala/CH2/Intro.scala
@@ -14,7 +14,38 @@ object MyProgram{
         msg.format(x, abs(x))
     }
 
-    @main def printAbs(): Unit = {
-        println(formatAbs(-42))
+    private def formatFactorial(n: Int) = {
+        val msg = "The factoral of %d is %d"
+        msg.format(n, factorial(n))
+    }
+
+    def formatResult(name: String, n: Int, f: Int => Int): String = {
+        val msg = "The %s of %d is %d"
+        msg.format(name, n, f(n))
+    }
+
+    @main def printAbsAndFactorial(): Unit = {
+        println(formatResult("absolute value", -42, abs))
+        println(formatResult("factorial", 7, factorial))
+        println(fib(7))
+    }
+
+    def factorial(n: Int): Int = {
+        @annotation.tailrec
+        def go(n: Int, acc: Int): Int = {
+            if (n <= 0) acc
+            else  go(n - 1, n * acc)
+        }
+        go(n, 1)
+    }
+
+    // Exercise 2.1
+    def fib(n: Int): Int = {
+        @annotation.tailrec
+        def loop(n: Int, first: Int, second: Int): Int = {
+            if (n <= 0) first
+            else loop(n-1, second, first + second)
+        }
+        loop(n ,0,1)
     }
 }
