52527ce1087b9a1300a49df82a69ba9d39e88df6
Ryan Murphy <rymurphy12@gmail.com>
2023-07-04 11:48:18 -0500

    Up to 3.6

--- src/main/scala/fpinscala2/datastructures/LinkedList.scala
+++ src/main/scala/fpinscala2/datastructures/LinkedList.scala
@@ -28,6 +28,9 @@ object List {
     println(drop(tstLyst, 6)) // Nil
     println(drop(tstLyst, 1)) // List(2,3,4,5)
     println(drop(tstLyst, 5)) // Nil
+    println(dropWhile(tstLyst, x => x < 3))
+    println(dropWhile(tstLyst, x => x < 0))
+    println(dropWhile(tstLyst, x => x < 6))
   }
 
   // Exercise 3.2
@@ -52,6 +55,34 @@ object List {
       case (Nil, _) => Nil
       case (lyst, num) if(num <= 0) => lyst
       case (Cons(head, tail), num) => drop(tail, num - 1)
+    }
+  }
+
+  // Exercise 3.5
+  def dropWhile[A](as: List[A], f: A => Boolean): List[A] = {
+    as match {
+      case Nil => Nil
+      case Cons(head, tail) if(f(head)) => dropWhile(tail, f)
+      case _ => as
+    }
+  }
+
+  // Exercise 3.6
+  def init[A](as: List[A]): List[A] = {
+    as match {
+      case Cons(x, Cons(_, Nil)) => Cons(x, Nil)
+      case Nil => sys.error("Can't init an empty list")
+      case Cons(head, tail) => Cons(head, init(tail))
+    }
+    // Answer: it can't be constant time because a Linked List is head biased
+    // This requires us to go through each connected list until we find the last
+    // on to discard
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] = {
+    a1 match {
+      case Nil => a2
+      case Cons(head, tail) => Cons(head, append(tail, a2))
     }
   }
 
