a964ff32d6727cb04ffe1b45c94bfdaff3d1d546
Ryan Murphy <rymurphy12@gmail.com>
2023-09-09 21:33:54 -0500

    Completed Option Section of Chapter 4. Exercises 4.3 - 4.5

--- src/main/scala/fpinscala2/errorhandling/Option.scala
+++ src/main/scala/fpinscala2/errorhandling/Option.scala
@@ -46,4 +46,39 @@ enum Option[+A]{
 		// Update: According to the book it is correct
 		mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 	}
+
+}
+
+object Option {
+
+	// Exercise 4.3
+	def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+		(a,b) match {
+			case (_, None) => None
+			case (None, _) => None
+			case (Some(x), Some(y)) => Some(f(x, y))
+		}
+		// Alt solution in book
+		// a.flatMap(aa => b.map(bb => f(aa, bb)))
+	}
+
+	// Exercise 4.4
+	def sequence[A](as: List[Option[A]]): Option[List[A]] = {
+		// Ehh....I have my doubts. This is either not right or
+		// unnecessarily complex. Note: I used Scala's List class
+		// not the one I got from the book
+		as.foldRight(Some(Nil: List[A]))((aa, acc) => map2(aa, acc)(_ :: _))
+		// Update: I almost got it correct. For the z paramater I used
+		// None: Option[List[A]]. The correct answer is Some(Nil: List[A])
+		// I am assuming this would cause it to be None every time
+	}
+
+	// Exercise 4.5
+	def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
+		as.foldRight(Some(Nil: List[B]))((aa, bb) =>  map2(f(aa), bb)(_ :: _))
+	}
+
+	def sequenceViaTraverse[A](as: List[Option[A]]): Option[List[A]] = {
+		traverse(as)(x => x)
+	}
 }
