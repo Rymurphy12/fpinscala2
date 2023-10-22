7ef3c548185f5e822f21b9ced5efa239ddc2498c
Ryan Murphy <rymurphy12@gmail.com>
2023-10-22 18:52:43 -0500

    Completed up to exercise 5.3

--- src/main/scala/fpinscala2/laziness/LazyList.scala
+++ src/main/scala/fpinscala2/laziness/LazyList.scala
@@ -1,6 +1,7 @@
 package fpinscala2.laziness
 
 enum LazyList[+A]{
+	import LazyList._
 	case Empty
 	case Cons(h: () => A, t: () => LazyList[A])
 
@@ -8,6 +9,41 @@ enum LazyList[+A]{
 		this match {
 			case Empty => None
 			case Cons(h, t) => Some(h())
+		}
+	}
+
+	// Exercise 5.1
+	def toList: List[A] = {
+		@annotation.tailrec
+		def go(lzLyst: LazyList[A], acc: List[A]): List[A] = {
+			lzLyst match {
+				case Cons(h, t) => go(t(), h() :: acc)
+				case Empty => acc.reverse
+			}
+		}
+		go(this, Nil)
+	}
+
+	// Exerise 5.2
+	def take(n: Int): LazyList[A] = {
+		this match
+			case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
+			case _  => empty
+	}
+
+	// Exerise 5.2
+	def drop(n: Int): LazyList[A] = {
+		this match {
+			case Cons(h,t) if n > 0 => t().drop(n - 1)
+			case _ => empty
+		}
+	}
+
+	// Exercise 5.3
+	def takeWhile(p: A => Boolean): LazyList[A] = {
+		this match {
+			case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+			case _ => empty
 		}
 	}
 }
